1621407be4e99e21e2adeb218f2b5811c7c1618b
Matthew Toledo <matt.toledo@fundable.com>
2016-01-21 16:27:17 -0500

    added babelrc file

--- app/component.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-	var element = document.createElement('h1');
-	element.innerHTML = 'Hello World!!';
-	console.log(element);
-	return element;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -9,19 +9,22 @@ const PATHS = {
 	build: path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
 	// Entry accepts path or an object of entries.
 	// The build chapter contains an example of the latter
 	entry: PATHS.app,
+	// Add resolve.extensions. '' is needed to allow imports
+	// without an extension. Note the .'s before extensions!!!
+	// The matching will fail without!
+	resolve: {
+		extensions: ['','.js','.jsx']
+	},
 	output: {
 		path: PATHS.build,
 		filename: 'bundle.js'
 	},
-	plugins: [
-		new HtmlwebpackPlugin({
-			title: 'Kanban app'
-		})
-	],
 	module: {
 		loaders: [
 			{
@@ -30,9 +33,25 @@ const common = {
 				loaders: ['style','css'],
 				// Include accepts either a path or an array of paths
 				include: PATHS.app
+			},
+			// Set up jsx. This accepts js too thanks to RegExp
+			{
+				test: /\.jsx?$/,
+				// Enable caching for improved performance during development
+				// It uses default OS directory by default. If you need something
+				// more custom, pass a path to it.  I.e., babel?cacheDirectory=<path>
+				loaders: ['babel?cacheDirectory'],
+				include: PATHS.app
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlwebpackPlugin({
+			template: 'node_modules/html-webpack-template/index.html',
+			title: 'Kanban App',
+			appMountId: 'app'
+		})
+	]
 }
 
 // Default configuration
@@ -55,7 +74,7 @@ if (TARGET === 'start' || !TARGET) {
 			port: process.env.PORT
 		},
 	    plugins: [
-	      new webpack.HotModuleReplacementPlugin()
+			new webpack.HotModuleReplacementPlugin()
 	    ]
 	});
 }
